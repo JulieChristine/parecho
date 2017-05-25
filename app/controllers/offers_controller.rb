d7c0118f0995cb963b78bef29ffd3e319677d07d
JulieChristine <julie.wolsak@gmail.com>
2017-05-25 15:04:06 +0800

    add CRUD functionality to offer

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -15,6 +15,28 @@ class OffersController < ApplicationController
       end
     end
 
+    def edit
+      @shop = Shop.find(params[:shop_id])
+      @offer = Offer.find(params[:id])
+    end
+
+    def update
+      @shop = Shop.find(params[:shop_id])
+      @offer = Offer.find(params[:id])
+
+      if @offer.update(offer_params)
+        redirect_to shop_path(@offer.shop)
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @offer = Offer.find(params[:id])
+      @offer.destroy
+      redirect_to shop_path(@offer.shop)
+    end
+
     private
     def offer_params
       params.require(:offer).permit(:name, :description, :picture, :saving, :validity)
